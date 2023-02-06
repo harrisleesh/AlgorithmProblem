7023c9aabc2e82895a97dc964d0fc523f0e6d752
harris.yi <harris.yi@kakaostyle.com>
2023-02-06 23:52:28 +0900

    feat: Optimize MostWater

--- src/main/kotlin/MostWater.kt
+++ src/main/kotlin/MostWater.kt
@@ -1,4 +1,5 @@
 import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     println("Hello main")
@@ -26,6 +27,19 @@ fun maxArea(height: IntArray): Int {
                 maxArea = area
             }
         }
+    }
+    return maxArea
+}
+
+fun maxAreaOpt(height: IntArray): Int {
+    var j = height.size - 1
+    var i = 0
+    var maxArea = 0
+
+    while (i < j){
+        val actualHeight = min(height[i], height[j])
+        maxArea = max(maxArea, actualHeight * (j - i))
+        if(height[i] == actualHeight) i ++ else j--
     }
     return maxArea
 }
